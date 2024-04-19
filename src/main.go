7e1a92beae7981d6b43ec5034071d8ce555fd5e9
Oliver Salman <oliversalman1@gmail.com>
2024-04-19 14:21:04 +1000

    redundant

--- src/main.go
+++ src/main.go
@@ -10,28 +10,6 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-// func processTopMil(rc io.Reader) (ch chan []string) {
-// 	ch = make(chan []string, 10)
-// 	go func() {
-// 		r := csv.NewReader(rc)
-// 		if _, err := r.Read(); err != nil { //read header
-// 			log.Fatal(err)
-// 		}
-// 		defer close(ch)
-// 		for {
-// 			rec, err := r.Read()
-// 			if err != nil {
-// 				if err == io.EOF {
-// 					break
-// 				}
-// 				log.Fatal(err)
-// 			}
-// 			ch <- rec
-// 		}
-// 	}()
-// 	return
-// }
-
 func ReadCsv(filename string) ([][]string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -57,7 +35,7 @@ func main() {
 		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
 
-	records, err := ReadCsv("/Users/oliver/Documents/Personal/Programming/search-engine-crawler/top-1m.csv")
+	records, err := ReadCsv("top-1m.csv")
 	if err != nil {
 		log.Fatal("Failed to read CSV: ", err)
 	}
