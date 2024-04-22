2cc81088a7ff6c299626dfc09537776bd7efba55
Oliver Salman <oliversalman1@gmail.com>
2024-04-22 13:59:27 +1000

    loading logic

--- go.mod
+++ go.mod
@@ -4,8 +4,19 @@ go 1.22.0
 
 require (
 	github.com/gocolly/colly/v2 v2.1.0
+	github.com/gofrs/uuid v4.0.0+incompatible
 	github.com/jackc/pgx/v4 v4.18.3
-	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
 )
 
 require (
@@ -17,19 +28,10 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.14.3 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
-	github.com/jackc/pgtype v1.14.3 // indirect
-	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
-	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
--- src/main.go
+++ src/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"context"
 	"encoding/csv"
-	// "fmt"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 	"time"
+	"unicode"
 
 	"github.com/gocolly/colly/v2"
+	"github.com/gofrs/uuid"
+	"github.com/jackc/pgx/v4"
 )
+
+var (
+	user = os.Getenv("DB_USER")
+	host = os.Getenv("DB_HOST")
+	dbname = os.Getenv("DB_NAME")
+	password = os.Getenv("DB_PASSWORD")
+	port = os.Getenv("DB_PORT")
+)
+
+var lemmatisedMap map[string]string
+
+func init() {
+	data, err := os.ReadFile("lemmatised.json")
+	if err != nil {
+		log.Fatal("Failed to read lemmatised.json: ", err)
+	}
+	err = json.Unmarshal(data, &lemmatisedMap)
+	if err != nil {
+		log.Fatal("Failed to unmarshal lemmatised.json: ", err)
+	}
+}
 
 func ReadCsv(filename string) ([][]string, error) {
 	file, err := os.Open(filename)
@@ -20,28 +48,151 @@ func ReadCsv(filename string) ([][]string, error) {
 	return csv.NewReader(file).ReadAll()
 }
 
+func connectDB() (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbname))
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func extractMetaDescription(e *colly.HTMLElement) string {
+	return e.ChildAttr("meta[name=description]", "content")
+}
+
+func tokenize(text string) []string {
+    text = strings.ToLower(text)
+
+    re := regexp.MustCompile(`[\W_]+`) // Matches non-word characters and underscores
+    tokens := re.Split(text, -1)
+
+    var filteredTokens []string
+    for _, token := range tokens {
+        if !isEmptyOrSpace(token) { 
+            filteredTokens = append(filteredTokens, token)
+        }
+    }
+    return filteredTokens
+}
+
+func isEmptyOrSpace(str string) bool {
+    for _, r := range str {
+        if !unicode.IsSpace(r) {
+            return false
+        }
+    }
+    return true
+}
+
+func extractKeywords(text string) map[string]int {
+	words := tokenize(text)
+	wordCounts := make(map[string]int)
+	for _, word := range words {
+		if lemma, found := lemmatisedMap[word]; found {
+            wordCounts[lemma]++
+		}
+	}
+	return wordCounts
+}
+
+func lemmatize(keywords map[string]int) []string {
+	return []string{}
+}
+
+func storeData(db *pgx.Conn, title, description, text, url string, keywords []string) error {
+    tx, err := db.Begin(context.Background()) // Start a transaction
+    if err != nil {
+        return err
+    }
+    defer tx.Rollback(context.Background())  // Rollback if an error occurs
+
+    // 1. Insert into 'websites' table
+    var websiteID uuid.UUID
+    err = tx.QueryRow(context.Background(), `
+        INSERT INTO websites (title, description, url, word_count, rank) 
+        VALUES ($1, $2, $3, $4, $5) 
+        RETURNING id`, 
+        title, description, url, calculateWordCount(text), calculateRank(url),
+    ).Scan(&websiteID)
+    if err != nil {
+        return err
+    }
+
+    // 2. Insert into 'keywords' and 'website_keywords'
+    for _, word := range keywords {
+        // Upsert logic for keywords
+        var keywordID uuid.UUID
+        err = tx.QueryRow(context.Background(), `
+                INSERT INTO keywords (word, documents_containing_word) 
+                VALUES ($1, 1)  
+                ON CONFLICT (word) DO UPDATE SET documents_containing_word = keywords.documents_containing_word + 1
+                RETURNING id`, word).Scan(&keywordID)
+        if err != nil {
+            return err
+        }
+
+        // Insert into website_keywords
+        _, err = tx.Exec(context.Background(), `
+                INSERT INTO website_keywords (keyword_id, website_id, occurrences, position)
+                VALUES ($1, $2, $3, $4)`, 
+                keywordID, websiteID, countKeywordOccurrences(text, word), calculatePosition(text, word),
+        )
+        if err != nil {
+            return err
+        }
+    }
+
+    err = tx.Commit(context.Background()) // Commit transaction
+    return err
+}
+
+
+
+
 func main() {
+	csvData, err := ReadCsv("top-1m.csv")
+	if err != nil {
+		log.Fatal("Failed to read CSV: ", err)
+	}
+
+	db, err := connectDB()
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+	defer db.Close(context.Background())
+
 	c := colly.NewCollector(colly.Async(true))
-
 	c.IgnoreRobotsTxt = false
-
 	c.Limit(&colly.LimitRule{
 		DomainGlob:   "*",
 		Parallelism:  10,
 		RandomDelay:  1 * time.Second,
 	})
 
-	c.OnError(func(r *colly.Response, err error) {
-		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		title := e.ChildText("title")
+		description := extractMetaDescription(e)
+		url := e.Request.URL.String()
+		text := e.Text
+
+		// Keyword extraction and lemmatization
+		keywords := extractKeywords(text)
+		lemmatizedKeywords := lemmatize(keywords)
+
+		// Database operations
+		err := storeData(db, title, description, text, url, lemmatizedKeywords)
+		if err != nil {
+			log.Println("Error storing:", url, err)
+		}
 	})
 
-	records, err := ReadCsv("top-1m.csv")
-	if err != nil {
-		log.Fatal("Failed to read CSV: ", err)
-	}
-	for _, record := range records {
-		url := "https://" + record[1] + "/"
-		c.Visit(url)
+	c.OnRequest(func(r *colly.Request) {
+	fmt.Println("Visiting:", r.URL)
+	})
+
+	// Start crawling from the URLs in the CSV
+	for _, row := range csvData {
+		c.Visit(row[1]) 
 	}
 	c.Wait()
 }
